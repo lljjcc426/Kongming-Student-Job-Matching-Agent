844b5e768d24ea9f0f1201bbb0ab81705e9e21aa
lljjcc426 <204199163+lljjcc426@users.noreply.github.com>
2026-06-11 15:48:03 +0800

    feat: add 2d interviewer avatar

--- src/components/interview/InterviewerAvatar.tsx
+++ src/components/interview/InterviewerAvatar.tsx
@@ -1,36 +1,10 @@
 import type { AvatarSpeechState } from "../../types/interview";
+import TwoDInterviewerAvatar from "./TwoDInterviewerAvatar";
 
 type InterviewerAvatarProps = {
   state: AvatarSpeechState;
 };
 
 export default function InterviewerAvatar({ state }: InterviewerAvatarProps) {
-  return (
-    <div className={`interviewer-avatar ${state}`} aria-label="AI 面试官数字人">
-      <div className="avatar-halo" />
-      <div className="avatar-shoulders" />
-      <div className="avatar-head">
-        <div className="avatar-hair" />
-        <div className="avatar-face-soft-light" />
-        <span className="avatar-eye left" />
-        <span className="avatar-eye right" />
-        <span className="avatar-brow left" />
-        <span className="avatar-brow right" />
-        <span className="avatar-nose" />
-        <span className="avatar-mouth" />
-        <span className="avatar-ear left" />
-        <span className="avatar-ear right" />
-      </div>
-      <div className="avatar-neck" />
-      <div className="avatar-suit">
-        <span />
-        <i />
-      </div>
-      <div className="avatar-status-orbit">
-        <i />
-        <i />
-        <i />
-      </div>
-    </div>
-  );
+  return <TwoDInterviewerAvatar status={state} className="interviewer-avatar" />;
 }
